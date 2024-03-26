a905fe69f49f4cd842c3fd3a32b3c3cd81baefa3
Clare Zhu <clare.zhu@mail.utoronto.ca>
2024-03-26 14:02:17 -0400

    added stt file

---
+++ soar_9/frontend/app/STT.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import Voice from '@react-native-voice/voice';
+
+
+
+export default function STT() {
+
+
+    let [started, setStarted] = useState(false);
+    let [results, setResults] = useState([]);
+
+    useEffect(() => {
+      Voice.onSpeechError = onSpeechError;
+      Voice.onSpeechResults = onSpeechResults;
+
+      return () => {
+        Voice.destroy().then(Voice.removeAllListeners);
+      }
+    }, []);
+
+    const startSpeechToText = async () => {
+      await Voice.start("en-NZ");
+      setStarted(true);
+    };
+
+    const stopSpeechToText = async () => {
+      await Voice.stop();
+      setStarted(false);
+    };
+
+    const onSpeechResults = (result) => {
+      setResults(result.value);
+    };
+
+    const onSpeechError = (error) => {
+      console.log(error);
+    };
+
+    return (
+      <View style={styles.container}>
+        {!started ? <Button title='Start Speech to Text' onPress={startSpeechToText} /> : undefined}
+        {started ? <Button title='Stop Speech to Text' onPress={stopSpeechToText} /> : undefined}
+        {results.map((result, index) => <Text key={index}>{result}</Text>)}
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
